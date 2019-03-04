e5cac1192cf50267ab8bc4fe57f0e7eb1c7caf39
Sean Jones <sean.d.jones92@gmail.com>
2019-03-03 20:16:29 -0700

    started adding function library to run commands against postgres

---
+++ todo/todopostgres/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import psycopg2
+
+class connection(object):
+    def __init__(self):
+        self.dbname = str(os.getenv('PSQL_DBNAME'))
+        self.user = str(os.getenv('PSQL_DBNAME'))
+        self.password = str(os.getenv('PSQL_PASSWORD'))
+        self.host = str(os.getenv('PSQL_HOST'))
+        self.port = str(os.getenv('PSQL_PORT'))
+        self.connstring = "dbname=%s user=%s password=%s host=%s port=%s" % (self.dbname, self.user, self.password, self.host, self.port)
+
+    def getmostrecent(self):
+        conn = psycopg2.connect(self.connstring)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM todo;")
+        mostrecent = cur.fetchone()
+        cur.close()
+        conn.close()
+        return mostrecent
