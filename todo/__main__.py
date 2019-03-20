7f9cbe958e34bd63f16bb78a0a20e27f7d9a14e3
Sean Jones <jonessean@localhost.localdomain>
2019-03-20 17:45:21 -0600

    made some formatting changes

--- todo/__main__.py
+++ todo/__main__.py
@@ -5,15 +5,18 @@ import todopsql
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def hello():
-    return "Hello World!" # This page should serv as API documentation
+    return "Hello World!"  # This page should serv as API documentation
+
 
 @app.route("/getall", methods=['GET'])
 def getall():
     tododb = todopsql.connection()
     response = jsonify(tododb.getall())
     return response
+
 
 @app.route("/createtask", methods=["POST"])
 def createtask():
@@ -27,6 +30,7 @@ def createtask():
         response = "Task created successfully"
         return response
 
+
 @app.route("/completetask", methods=["PUT"])
 def completetask():
     if (request.method == 'PUT') and (request.is_json):
@@ -36,6 +40,7 @@ def completetask():
         tododb.completetask(taskid)
         response = "Task completed"
         return response
+
 
 @app.route("/deletetask", methods=["DELETE"])
 def deletetask():
@@ -47,12 +52,14 @@ def deletetask():
         response = "Task deleted"
         return response
 
+
 @app.route("/echo", methods=["POST"])
 def echorequest():
     if (request.method == 'POST') and (request.is_json):
         content = request.get_json()
         response = jsonify(content)
         return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
--- todo/todopsql/__init__.py
+++ todo/todopsql/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-import json
 import datetime
 import psycopg2
+
 
 class connection(object):
     def __init__(self):
@@ -12,7 +12,8 @@ class connection(object):
         self.password = str(os.getenv('PSQL_PASSWORD'))
         self.host = str(os.getenv('PSQL_HOST'))
         self.port = str(os.getenv('PSQL_PORT'))
-        self.connstring = "dbname=%s user=%s password=%s host=%s port=%s" % (self.dbname, self.user, self.password, self.host, self.port)
+        self.connstring = "dbname=%s user=%s password=%s host=%s port=%s" % \
+            (self.dbname, self.user, self.password, self.host, self.port)
 
     def getall(self):
         # Create connection and get cursor
@@ -25,17 +26,21 @@ class connection(object):
         cur.close()
         conn.close()
         return mostrecent
-    
+
     def createtask(self, title, body, duedate):
         # Create connection and get cursor
         conn = psycopg2.connect(self.connstring)
         cur = conn.cursor()
         # Interact with DB
         if None not in (duedate['year'], duedate['month'], duedate['day']):
-            cur.execute("INSERT INTO todo (title, body, createdon, duedate) VALUES ('%s', '%s', current_date, '%s');" 
-                        % (title, body, datetime.date(duedate['year'], duedate['month'], duedate['day'])))
+            cur.execute("INSERT INTO todo (title, body, createdon, duedate) " +
+                        "VALUES ('%s', '%s', current_date, '%s');"
+                        % (title, body, datetime.date(duedate['year'],
+                                                      duedate['month'],
+                                                      duedate['day'])))
         else:
-            cur.execute("INSERT INTO todo (title, body, createdon) VALUES ('%s', '%s', current_date);" 
+            cur.execute("INSERT INTO todo (title, body, createdon) VALUES " +
+                        "('%s', '%s', current_date);"
                         % (title, body))
         # Close cursor and connection
         cur.close()
@@ -49,7 +54,8 @@ class connection(object):
         conn = psycopg2.connect(self.connstring)
         cur = conn.cursor()
         # Interact with DB
-        cur.execute("UPDATE todo SET completedon = current_date WHERE id = %s;" % (taskid))
+        cur.execute("UPDATE todo SET completedon = current_date WHERE id = %s;"
+                    % (taskid))
         # Close cursor and connection
         cur.close()
         conn.commit()
