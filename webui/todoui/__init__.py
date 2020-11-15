42475f36660020a8201449bba60f596507818d30
Sean Jones <sean.d.jones92@gmail.com>
2020-11-15 18:54:16 +0000

    webui components and debugging worked

--- webui/todoui/__init__.py
+++ webui/todoui/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('base.html')
